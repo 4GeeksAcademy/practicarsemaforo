389566060314b34ac18fae9270bd6b18264240ae
javier5pe <72091566+javier5pe@users.noreply.github.com>
2024-06-14 18:53:05 +0000

    semaforo

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -6,9 +6,9 @@ const Semaforo = () => {
 
     return (
         <div className="semaforo">
-            <div onClick={()=>setColor("verde")}   className={"faro verde " + ((color==="verde") ? "encendido":"") }></div>
+            <div onClick={()=>setColor("verde")}   className={"faro verde "   + ((color==="verde")   ? "encendido":"") }></div>
             <div onClick={()=>setColor("naranja")} className={"faro naranja " + ((color==="naranja") ? "encendido": "")}></div>
-            <div onClick={()=>setColor("rojo")}     className={"faro rojo " + ((color==="rojo,") ? "encendido": "")}></div>
+            <div onClick={()=>setColor("rojo")}    className={"faro rojo "    + ((color==="rojo")   ? "encendido": "")}></div>
 
         </div>            
                                                                                                                    
